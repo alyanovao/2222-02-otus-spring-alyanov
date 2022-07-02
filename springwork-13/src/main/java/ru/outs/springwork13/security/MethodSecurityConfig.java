05ac344710571e68df1a8165c69a27eebaca15b7
Альянов Артем Олегович <domovoy175@gmail.com>
2022-07-02 13:10:39 +0300

    Убрал не используемый функционал

--- springwork-13/src/main/java/ru/outs/springwork13/security/MethodSecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package ru.outs.springwork13.security;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class MethodSecurityConfig {
-}
