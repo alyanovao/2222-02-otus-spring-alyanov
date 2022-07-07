76d42f90ca7df47d35211006fbd989411a5e5aa9
Альянов Артем Олегович <domovoy175@gmail.com>
2022-07-07 21:02:16 +0300

    add test

--- springwork-14/src/test/java/ru/otus/springwork14/config/BatchConfigTest.java
+++ springwork-14/src/test/java/ru/otus/springwork14/config/BatchConfigTest.java
@@ -1,14 +1,16 @@
 package ru.otus.springwork14.config;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static ru.otus.springwork14.common.Constant.IMPORT_JOB;
 
 @SpringBootTest
@@ -18,11 +20,23 @@ class BatchConfigTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private JobRepositoryTestUtils jobRepository;
+
+    @BeforeEach
+    void clear() {
+        jobRepository.removeJobExecutions();
+    }
+
     @Test
-    void migrateStep() {
+    void migrateStep() throws Exception {
         Job job = jobLauncherTestUtils.getJob();
         assertThat(job).isNotNull()
                 .extracting(Job::getName)
                 .isEqualTo(IMPORT_JOB);
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+
     }
 }
