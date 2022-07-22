f73299ebfe938b40868bae57018e5db48b60a049
Альянов Артем Олегович <domovoy175@gmail.com>
2022-07-22 03:13:26 +0300

    Добавил реализацию apache camel

--- springwork-15/spring-integration/src/main/java/ru/otus/springintegration/SpringIntegrationApplication.java
+++ springwork-15/spring-integration/src/main/java/ru/otus/springintegration/SpringIntegrationApplication.java
@@ -3,10 +3,7 @@ package ru.otus.springintegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.otus.springintegration.gateway.SendServiceGateWay;
 import ru.otus.springintegration.service.ControlService;
-
-import java.io.File;
 
 @SpringBootApplication
 public class SpringIntegrationApplication {
